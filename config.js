434262e351160c9f67548bc1d823117fd285c894
freddyhutauruk18-cpu <freddyhutauruk18@gmail.com>
2026-01-22 16:08:35 +0700

    Add database links configuration in config.js

--- config.js
+++ config.js
@@ -1,1 +1,25 @@
+/* CONFIG.JS - Database Links Configuration */
 
+// Menggunakan Link Raw Repository (Lebih stabil & satu paket dengan project)
+const GIST_URLS = [
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-01.json", // DIV 01
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-02.json", // DIV 02
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-03.json", // DIV 03
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-04.json", // DIV 04
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-05.json", // DIV 05
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-06.json", // DIV 06
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-07.json", // DIV 07
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-08.json", // DIV 08
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-09.json", // DIV 09
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-10.json", // DIV 10
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-11.json", // DIV 11
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-12.json", // DIV 12
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-13.json", // DIV 13
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-14.json", // DIV 14
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-15.json", // DIV 15
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-16.json", // DIV 16
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-17.json", // DIV 17
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-18.json", // DIV 18
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-19.json", // DIV 19
+    "https://raw.githubusercontent.com/freddyhutauruk18-cpu/rab/main/AHSP-20.json"  // DIV 20
+];
